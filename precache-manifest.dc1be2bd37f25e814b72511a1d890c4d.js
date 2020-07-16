16df325a2c528b9693c4b717f8af86d0746742fc
yeji <hahahayeji@gmail.com>
2020-07-16 14:00:55 +0900

    Updates

---
+++ precache-manifest.dc1be2bd37f25e814b72511a1d890c4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd198f942782136c994c8eb55d49c55",
+    "url": "/wedding-invitation-web/index.html"
+  },
+  {
+    "revision": "c001d2842225590836cc",
+    "url": "/wedding-invitation-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d058420383693343ffdd",
+    "url": "/wedding-invitation-web/static/js/2.e16f8ef6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wedding-invitation-web/static/js/2.e16f8ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c001d2842225590836cc",
+    "url": "/wedding-invitation-web/static/js/main.f52154bd.chunk.js"
+  },
+  {
+    "revision": "98a8c8a0dded1abf5270",
+    "url": "/wedding-invitation-web/static/js/runtime-main.0afb47aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wedding-invitation-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
